250625905d127c517a553a78bfdc1b85d00e776c
MONDEM-DILEEP <24331a05i4@mvgrce.edu.in>
2025-12-16 09:16:31 +0530

    Add files via upload

---
+++ TYPE.py
@@ -0,0 +1,14 @@
+num=30
+flo=30.23251
+
+print("before type conversion number : ",num)
+print("before type conversion float : ",flo)
+
+num=str(num)
+flo=int(flo)
+
+print("lower datatype to upper datatype")
+print("after type conversion number : ",num)
+
+print("upper datatype to lower datatype")
+print("after type conversion float : ",flo)
